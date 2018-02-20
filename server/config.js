5879ea2ee661cb0b0ff4d23d90f272aea1029989
Immad Hamid <immad.hamid@gmail.com>
2018-02-20 11:34:49 +0500

    Configured Nodejs Server with required Dependencies and Setup Angular for the Front-End App

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://immad:immad@ds243008.mlab.com:43008/amazonowebapplication',
+    port: 3030,
+    secret: 'ImmadHamid3102075807'
+};
---
+++ server/server.js
@@ -0,0 +1,38 @@
+// requiring 3rd party modules
+const express       = require('express');
+const morgan        = require('morgan');
+const bodyParser    = require('body-parser');
+const mongoose      = require('mongoose');
+const cors          = require('cors');
+// requiring local files
+const config        = require('./config');
+// intialting express
+const app           = express();
+
+// connecting mongoDB using mongoose
+mongoose.connect(config.database, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Connected to the database');
+    }
+});
+// using body parser for json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// adding middleware
+app.use(morgan('dev'));
+// adding cors to communicate with the client
+app.use(cors());
+
+// test route
+app.get('/', (req, res, next) => {
+    res.json({
+        user: 'Immad Hamid'
+    });
+})
+
+// the port at which the server would be running
+app.listen(3030, err => {
+    console.log(`Working on port ${ config.port }`);
+});
